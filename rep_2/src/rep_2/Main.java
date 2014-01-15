a2a36dbf5fb486e583efb51b2295e636399abbb5
Antonio <Antonio@antoniohc>
2014-01-15 13:59:43 +0100

    changelog 1.0: 	- se añade la segunda línea de codigo

--- rep_2/src/rep_2/Main.java
+++ rep_2/src/rep_2/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("esto es una línea de código");
+		System.out.println("segunda línea de código de la versión 1.0");
 	}
 
 }
